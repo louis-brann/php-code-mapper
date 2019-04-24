6592a9c8091b99b536007356bc44b8ea66e45a5c
Louie Brann <louis.brann@gmail.com>
2019-04-24 17:02:34 -0600

    find-function-definition

--- src/find-function-definition.php
+++ src/find-function-definition.php
@@ -1,3 +1,18 @@
 <?php
 
-// TODO: recover lost script :'(
+// TODO change to namespaces ASAP
+require_once(__DIR__ . '/CodeMapper.php');
+
+$rootDir = $argv[1] ?? null;
+$qualifiedFunction = $argv[2] ?? null;
+$callerFile = $argv[3] ?? null;
+if (is_null($rootDir) || is_null($qualifiedFunction) || is_null($callerFile)) {
+  print "Not enough arguments." . PHP_EOL;
+  print "Usage: php find-function-definition.php <rootDir> <function> <callerFile>" . PHP_EOL;
+}
+
+$mapper = new CodeMapper($rootDir);
+$mapper->simulateScopeOfFile($callerFile);
+$definitionFile = $mapper->getFunctionDefinitionFile($qualifiedFunction);
+
+print $definitionFile;
